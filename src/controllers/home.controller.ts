0d48b558a404236786ba949ad2db13badbf3b5b3
Jakub Solecki <jsolecki@pgs-soft.com>
2020-02-19 13:19:10 +0100

    error handlers

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response } from 'express';
+import HttpException from '../exceptions/http-exception';
 
 const router = Router();
 
 router.get('/', (_: Request, response: Response) => {
+  throw new HttpException(400, 'home');
   response.send('home');
 });
 
