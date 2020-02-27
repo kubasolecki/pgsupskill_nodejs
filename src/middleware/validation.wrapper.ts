bf47927b79950c3b8ab464f72395dd7530e38e39
Lukasz Wojcik (PGS Software) <51819432+lwojcikpgs@users.noreply.github.com>
2020-02-27 14:08:16 +0100

    option 2

--- src/middleware/validation.wrapper.ts
+++ src/middleware/validation.wrapper.ts
@@ -20,18 +20,19 @@ export function validationWrapper<T, K>(
       next: NextFunction
     ) => {
       const classObject = plainToClass(type, request.body);
-      validate(classObject, { skipMissingProperties })
-      .then(async (errors) => {
-        if (errors.length > 0) {
-          const message = errors
-          .map((error: ValidationError) => Object.values(error.constraints))
-          .join(', ');
-          next(new HttpException(BAD_REQUEST, message));
-        } else {
-          request.model = classObject;
-          return await controller(request, response, next);
-        }
+      const errors = await validate(classObject, {
+        skipMissingProperties,
       });
+
+      if (errors.length === 0) {
+        request.model = classObject;
+        return await controller(request, response, next);
+      }
+
+      const message = errors
+        .map((error: ValidationError) => Object.values(error.constraints))
+        .join(', ');
+      throw new HttpException(BAD_REQUEST, message);
     }
   );
 }
